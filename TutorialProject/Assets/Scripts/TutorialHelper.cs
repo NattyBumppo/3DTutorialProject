8b9f928560ddbd7d116fca798471b34a5195d5d1
Nat Guy <nattybumppo@gmail.com>
2017-08-02 23:18:34 -0700

    First commit.

---
+++ TutorialProject/Assets/Scripts/TutorialHelper.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialHelper : MonoBehaviour {
+
+    public bool showNormals;
+    public bool showVertices;
+    public bool showBoundingBox;
+
+    public float vertexRadius;
+    public float normalLength;
+
+    private List<Material> initialMaterials = new List<Material>();
+    public Material uvMaterial;
+    public Material vertexTweakingMaterial;
+    public Material furMaterial;
+
+    public List<MeshFilter> meshFilters = new List<MeshFilter>();
+    public List<MeshRenderer> meshRenderers = new List<MeshRenderer>();
+
+    public Text meshSummaryText;
+
+    void Start()
+    {
+        for (int i = 0; i < meshRenderers.Count; i++)
+        {
+            MeshRenderer mr = meshRenderers[i];
+            initialMaterials.Add(mr.sharedMaterial);
+        }
+
+        meshSummaryText.text = SummarizeMeshes();
+	}
+	
+    private string SummarizeMeshes()
+    {
+        string summaryText = "";
+
+        for (int i = 0; i < meshFilters.Count; i++)
+        {
+            summaryText += "<b>" + meshFilters[i].gameObject.name + "</b>\n";
+            summaryText += "Vertex Count: " + meshFilters[i].sharedMesh.vertexCount + "\n";
+            summaryText += "Triangle Count: " + meshFilters[i].sharedMesh.triangles.Length / 3 + "\n\n";
+        }
+
+        return summaryText;
+    }
+
+	// Update is called once per frame
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            ApplyUvMaterial();
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ApplyInitialMaterials();
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            ApplyVertexTweakingMaterial();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ApplyFurMaterial();
+        }
+    }
+
+    private void ApplyInitialMaterials()
+    {
+        for (int i = 0; i < meshRenderers.Count; i++)
+        {
+            MeshRenderer mr = meshRenderers[i];
+            mr.sharedMaterial = initialMaterials[i];
+        }
+    }
+
+    private void ApplyUvMaterial()
+    {
+        for (int i = 0; i < meshRenderers.Count; i++)
+        {
+            MeshRenderer mr = meshRenderers[i];
+            mr.sharedMaterial = uvMaterial;
+        }
+    }
+
+    private void ApplyVertexTweakingMaterial()
+    {
+        for (int i = 0; i < meshRenderers.Count; i++)
+        {
+            MeshRenderer mr = meshRenderers[i];
+            mr.sharedMaterial = vertexTweakingMaterial;
+        }
+    }
+
+    private void ApplyFurMaterial()
+    {
+        for (int i = 0; i < meshRenderers.Count; i++)
+        {
+            MeshRenderer mr = meshRenderers[i];
+            mr.sharedMaterial = furMaterial;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (showVertices)
+        {
+            for (int i = 0; i < meshFilters.Count; i++)
+            {
+                Transform t = meshFilters[i].transform;
+                for (int j = 0; j < meshFilters[i].sharedMesh.vertices.Length; j++)
+                {
+                    Vector3 vertex = meshFilters[i].sharedMesh.vertices[j];
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawSphere(t.TransformPoint(vertex), vertexRadius);
+                }
+
+            }
+        }
+
+        if (showNormals)
+        {
+            for (int i = 0; i < meshFilters.Count; i++)
+            {
+                Transform t = meshFilters[i].transform;
+                for (int j = 0; j < meshFilters[i].sharedMesh.vertices.Length; j++)
+                {
+                    Vector3 vertex = meshFilters[i].sharedMesh.vertices[j];
+                    Vector3 normal = meshFilters[i].sharedMesh.normals[j];
+                    Gizmos.color = Color.magenta;
+                    Gizmos.DrawLine(t.TransformPoint(vertex), t.TransformPoint(vertex + normal * normalLength * (1.0f / t.localScale.x)));
+                }
+            }
+        }
+
+        if (showBoundingBox)
+        {
+            for (int i = 0; i < meshFilters.Count; i++)
+            {
+                Gizmos.DrawWireCube(meshFilters[i].sharedMesh.bounds.center, meshFilters[i].sharedMesh.bounds.size);
+            }
+        }
+
+    }
+}
---
+++ TutorialProject/Assets/Shaders/VertexTweakingShader.shader
@@ -0,0 +1,79 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Custom/SampleVertexShader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+				float4 normal: NORMAL;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				
+				
+				// Alter vertex along normal
+				
+				
+				
+				//v.vertex = v.vertex + (sin(_Time)) * 0.2 * v.normal;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				//fixed4 col = tex2D(_MainTex, i.uv);
+
+				float x = sin(_Time * 20) * 0.5 + 0.5;
+
+				// If U < 0.5
+				if (i.uv.y < x)
+				{
+					// Draw green pixel
+					return float4(0, x, 0, 1);
+				}
+				else
+				{
+					// Draw purple pixel
+					return float4(1-x, 0, 1-x, 1);
+				}
+				//return col;
+			}
+			ENDCG
+		}
+	}
+}
